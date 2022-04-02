2e6da3d17aa3b8ebe9a12b124132b9d97cc1f051
匡一珉 <kuangyimin@xdf.cn>
2022-04-02 18:37:38 +0800

    add

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const rootEl = document.getElementById('root')
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootEl
 );
 
 // If you want to start measuring performance in your app, pass a function
